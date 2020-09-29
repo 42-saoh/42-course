74ff554a4a1a98cb2a7d4abd31e35e29e8902799
saoh <saoh@c2r2s7.42seoul.kr>
2020-09-29 15:44:02 +0900

    libft

---
+++ libft/root/ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saoh <saoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/29 14:38:50 by saoh              #+#    #+#             */
+/*   Updated: 2020/09/29 15:39:44 by saoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void		*ft_calloc(size_t elt_count, size_t elt_size)
+{
+	size_t	i;
+	void	*temp;
+
+	if (!(temp = (void *)malloc(elt_size * elt_count)))
+		return (NULL);
+	i = 0;
+	while (i < elt_count * elt_size)
+	{
+		*((char *)temp + i) = 0;
+		i++;
+	}
+	return (temp);
+}
---
+++ libft/root/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saoh <saoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/29 15:35:32 by saoh              #+#    #+#             */
+/*   Updated: 2020/09/29 15:43:41 by saoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char		*ft_strdup(const char *str)
+{
+	char	*clone;
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	if (!(clone = (char *)malloc(sizeof(char) * i)))
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		clone[i] = str[i];
+		i++;
+	}
+	clone[i] = 0;
+	return (clone);
+}
