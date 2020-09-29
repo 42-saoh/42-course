cd188229f8ed65c7da3d462b77097c0b32538017
saoh <saoh@c2r2s7.42seoul.kr>
2020-09-29 11:48:26 +0900

    libft

---
+++ root/ft_memcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saoh <saoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/29 10:20:56 by saoh              #+#    #+#             */
+/*   Updated: 2020/09/29 10:42:05 by saoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_memcmp(const void *ptr1, const void *ptr2, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (*((unsigned char *)ptr1 + i) > (*((unsigned char *)ptr2 + i)))
+			return (1);
+		else if (*((unsigned char *)ptr1 + i) < (*((unsigned char *)ptr2 + i)))
+			return (-1);
+		else
+			i++;
+	}
+	return (0);
+}
