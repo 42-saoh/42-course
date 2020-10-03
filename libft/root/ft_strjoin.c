62bfef11ebd1f13b9db21c65df8e9f00d52f8251
saoh <saoh@c2r2s7.42seoul.kr>
2020-10-03 21:28:02 +0900

    libft

---
+++ libft/root/ft_strjoin.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saoh <saoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/03 20:57:03 by saoh              #+#    #+#             */
+/*   Updated: 2020/10/03 21:16:33 by saoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static size_t	ft_strlen(char *str)
+{
+	size_t		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char			*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t		i;
+	size_t		s1_len;
+	size_t		s2_len;
+	char		*str;
+
+	i = 0;
+	s1_len = ft_strlen((char *)s1);
+	s2_len = ft_strlen((char *)s2);
+	if (!(str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1))))
+		return (NULL);
+	while (i < s1_len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < s2_len)
+	{
+		str[i + s1_len] = s2[i];
+		i++;
+	}
+	str[i + s1_len] = 0;
+	return (str);
+}
--- libft/root/ft_strtrim.c
+++ libft/root/ft_strtrim.c
@@ -1,30 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: saoh <saoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/03 19:46:12 by saoh              #+#    #+#             */
-/*   Updated: 2020/10/03 21:18:02 by saoh             ###   ########.fr       */
+/*   Created: 2020/10/03 21:18:36 by saoh              #+#    #+#             */
+/*   Updated: 2020/10/03 21:27:34 by saoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-
-char		*ft_substr(const char *str, unsigned int start, size_t len)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*substr;
-	size_t	i;
 
-	if (!(substr = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		substr[i] = str[i + start - 1];
-		i++;
-	}
-	substr[i] = 0;
-	return (substr);
 }
