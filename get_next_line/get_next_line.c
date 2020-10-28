960a109c2fea714260cfd34f54bd15887a948fe0
Saehoon Oh <saoh@c3r6s5.42seoul.kr>
2020-10-28 15:17:29 +0900

    gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: saoh <saoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 16:12:09 by saoh              #+#    #+#             */
-/*   Updated: 2020/10/27 19:01:16 by saoh             ###   ########.fr       */
+/*   Updated: 2020/10/28 14:39:52 by saoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,15 @@
 
 int		ft_read(char *rd_str, int fd)
 {
-	if (!(rd_str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
-		return (-1);
-	if (read(fd, rd_str, BUFFER_SIZE) == 0)
-	{
-		free(rd_str);
+	int a;
+
+	if ((a = read(fd, rd_str, BUFFER_SIZE + 1)) == 0)
 		return (0);
-	}
-	rd_str[BUFFER_SIZE] = 0;
+	rd_str[a] = 0;
 	return (1);
 }
 
-void	ft_put_str(char *rd_str, char *buff, char **line)
+void	*ft_put_str(char *rd_str, char *buff, char **line)
 {
 	char *t_str;
 	char *s_str;
@@ -37,7 +34,8 @@ void	ft_put_str(char *rd_str, char *buff, char **line)
 		s_str = ft_substr(rd_str, 0, t_str - rd_str);
 		*line = ft_strjoin(buff, s_str);
 		free(buff);
-		buff = ft_strdup(t_str + 1);
+		if ((t_str + 1) != NULL)
+			buff = ft_strdup(t_str + 1);
 	}
 	else
 	{
@@ -46,7 +44,31 @@ void	ft_put_str(char *rd_str, char *buff, char **line)
 		buff = ft_strjoin(s_str, rd_str);
 	}
 	free(s_str);
-	free(rd_str);
+	return (buff);
+}
+
+void	*ft_put_buff(char *buff, char **line)
+{
+	char *t_buff;
+	char *s_buff;
+
+	if (buff == NULL)
+		return (NULL);
+	s_buff = ft_strdup(buff);
+	if ((t_buff = ft_strchr(s_buff, '\n')) != NULL)
+	{
+		*line = ft_substr(buff, 0, t_buff - s_buff);
+		free(buff);
+		if ((t_buff + 1) != NULL)
+			buff = ft_strdup(t_buff + 1);
+	}
+	else
+	{
+		*line = ft_strdup(buff);
+		free(buff);
+	}
+	free(s_buff);
+	return (buff);
 }
 
 int		get_next_line(int fd, char **line)
@@ -55,12 +77,16 @@ int		get_next_line(int fd, char **line)
 	char		*rd_str;
 	int			a;
 
-	rd_str = 0;
-	a = ft_read(rd_str, fd);
-	if (a == -1)
+	if (line == NULL)
+		return (-1);
+	if (!(rd_str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	if ((a = ft_read(rd_str, fd)) == -1)
 		return (a);
-	else if (a == 0)
-		return (a);
-	ft_put_str(rd_str, buff, line);
+	if (a == 0)
+		buff = ft_put_buff(buff, line);
+	else
+		buff = ft_put_str(rd_str, buff, line);
+	free(rd_str);
 	return (a);
 }
