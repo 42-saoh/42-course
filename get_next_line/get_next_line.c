3cff59e68202f9fe157a8eab56c7ed80f9b2e08f
Saehoon Oh <saoh@c4r3s4.42seoul.kr>
2020-10-30 21:21:49 +0900

    gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: saoh <saoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 16:12:09 by saoh              #+#    #+#             */
-/*   Updated: 2020/10/30 20:41:52 by saoh             ###   ########.fr       */
+/*   Updated: 2020/10/30 21:20:59 by saoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,8 @@ int		chr_buf(char **buff, char *chr_buff, char **line, int ret)
 
 int		chr_nul(char **buff, char **line)
 {
+	if (*buff == NULL)
+		*buff = ft_strdup("");
 	*line = *buff;
 	*buff = 0;
 	return (0);
@@ -43,21 +45,21 @@ int		chr_nul(char **buff, char **line)
 
 int		get_next_line(int fd, char **line)
 {
-	static char		*buff[OPEN_MAX];
+	static char		*buff[OPEN_MAX + 2];
 	char			rd_str[BUFFER_SIZE + 1];
 	char			*chr_buff;
 	int				ret;
 
 	if (fd < 0 || !line || BUFFER_SIZE <= 0)
 		return (-1);
-	while ((ret = read(fd, rd_str, BUFFER_SIZE)) >= 0)
+	while ((ret = read(fd, rd_str, BUFFER_SIZE)) > 0)
 	{
 		rd_str[ret] = 0;
 		buff[fd] = ft_strjoin(buff[fd], rd_str);
 		if ((chr_buff = ft_strchr(buff[fd], '\n')) != NULL)
 			return (chr_buf(&buff[fd], chr_buff, line, ret));
-		else if (chr_buff == NULL && ret == 0)
-			return (chr_nul(&buff[fd], line));
 	}
-	return (-1);
+	if (ret < 0)
+		return (-1);
+	return (chr_nul(&buff[fd], line));
 }
