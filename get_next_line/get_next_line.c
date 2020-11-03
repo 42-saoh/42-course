23129927626ea240349d1ca72952aca997a82e4e
Saehoon Oh <saoh@c4r3s4.42seoul.kr>
2020-11-03 21:00:31 +0900

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: saoh <saoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 16:12:09 by saoh              #+#    #+#             */
-/*   Updated: 2020/11/02 12:54:43 by saoh             ###   ########.fr       */
+/*   Updated: 2020/11/03 20:53:08 by saoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		chr_buf(char **buff, char *chr_buff, char **line)
+int		ft_put_buff(char **buff, char *chr_buff, char **line)
 {
 	char	*t_buff;
 
@@ -31,14 +31,14 @@ int		chr_buf(char **buff, char *chr_buff, char **line)
 	return (1);
 }
 
-int		chr_nul(char **buff, char **line)
+int		ft_eof_read(char **buff, char **line)
 {
 	char	*chr_buff;
 
 	if (*buff == NULL)
 		*buff = ft_strdup("");
 	else if ((chr_buff = ft_strchr(*buff, '\n')) != NULL)
-		return (chr_buf(buff, chr_buff, line));
+		return (ft_put_buff(buff, chr_buff, line));
 	*line = *buff;
 	*buff = 0;
 	return (0);
@@ -46,21 +46,21 @@ int		chr_nul(char **buff, char **line)
 
 int		get_next_line(int fd, char **line)
 {
-	static char		*buff[OPEN_MAX + 2];
+	static char		*buff[OPEN_MAX + 1];
 	char			rd_str[BUFFER_SIZE + 1];
 	char			*chr_buff;
 	int				ret;
 
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+	if (fd < 0 || !line || BUFFER_SIZE <= 0 || fd > OPEN_MAX)
 		return (-1);
 	while ((ret = read(fd, rd_str, BUFFER_SIZE)) > 0)
 	{
 		rd_str[ret] = 0;
 		buff[fd] = ft_strjoin(buff[fd], rd_str);
 		if ((chr_buff = ft_strchr(buff[fd], '\n')) != NULL)
-			return (chr_buf(&buff[fd], chr_buff, line));
+			return (ft_put_buff(&buff[fd], chr_buff, line));
 	}
 	if (ret < 0)
 		return (-1);
-	return (chr_nul(&buff[fd], line));
+	return (ft_eof_read(&buff[fd], line));
 }
