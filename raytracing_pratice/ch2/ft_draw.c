7fd79cb0a612204a58e7343ef7d8e767aaf2ee60
saoh <saoh@saohui-MacBookPro.local>
2021-01-03 18:08:47 +0900

    raytracing_pratice

--- raytracing_pratice/ch2/ft_draw.c
+++ raytracing_pratice/ch2/ft_draw.c
@@ -4,4 +4,17 @@ void			draw_gradaition(t_img_data *data)
 {
 	int			rgb;
 	t_vec		*color;
+	double		x;
+	double		y;
+
+	y = 0;
+	while (y < data->height)
+	{
+		x = 0;
+		while (x < data->width)
+		{
+			color = vec_create(x / data->width, y / data->height, 1.0 / 4.0);
+
+		}
+	}
 }
