0f66194c5036695c893a52e368a7c4335e05f002
saoh <saoh@student.42seoul.kr>
2021-05-12 19:35:17 +0900

    ft_service

--- ft_service/srcs/nginx/nginx.sh
+++ ft_service/srcs/nginx/nginx.sh
@@ -2,14 +2,14 @@
 
 rm /etc/nginx/conf.d/default.conf
 mv /etc/nginx/default.conf /etc/nginx/conf.d/
+
 mkdir -p /etc/nginx/ssl
 openssl req -new -x509 -nodes -newkey rsa:4096 -keyout localhost-nginx.key -out localhost-nginx.crt -days 365 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=saoh/CN=localhost"
 mv localhost-nginx.key /etc/nginx/ssl/nginx.key
 mv localhost-nginx.crt /etc/nginx/ssl/nginx.crt
 
-mkdir -p /run/nginx
-mkdir -p /var/www/html
-echo "<h1> WELLCOM NGINX </h1>" >> /var/www/html/index.html
+mkdir -p run/nginx
+ln -s /var/lib/nginx/html /var/www/
 
 /usr/sbin/sshd
 /usr/sbin/nginx -g "daemon off;"
