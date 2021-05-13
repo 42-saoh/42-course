69ca729fbb352240948d8406e12ba7eb8cba88e0
Saehoon Oh <saoh@c3r6s5.42seoul.kr>
2021-05-13 18:07:49 +0900

    ft_service

--- ft_service/srcs/nginx/nginx.sh
+++ ft_service/srcs/nginx/nginx.sh
@@ -4,12 +4,16 @@ rm /etc/nginx/conf.d/default.conf
 mv /etc/nginx/default.conf /etc/nginx/conf.d/
 
 mkdir -p /etc/nginx/ssl
-openssl req -new -x509 -nodes -newkey rsa:4096 -keyout localhost-nginx.key -out localhost-nginx.crt -days 365 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=saoh/CN=localhost"
-mv localhost-nginx.key /etc/nginx/ssl/nginx.key
-mv localhost-nginx.crt /etc/nginx/ssl/nginx.crt
+openssl req -new -x509 -nodes -newkey rsa:4096 -keyout localhost.key -out localhost.crt -days 365 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/CN=localhost"
+mv localhost.key /etc/nginx/ssl/
+mv localhost.crt /etc/nginx/ssl/
 
-mkdir -p run/nginx
-ln -s /var/lib/nginx/html /var/www/
+ssh-keygen -A
+adduser --disabled-password ${SSH_USERNAME}
+echo "${SSH_USERNAME}:${SSH_PASSWORD}" | chpasswd
+
+mkdir -p /run/nginx/
+echo "<h1>FT_SERVICES NGINX INDEX.HTML</h1>" >> /var/www/index.html
 
 /usr/sbin/sshd
 /usr/sbin/nginx -g "daemon off;"
