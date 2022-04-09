0810cf42db6c26d031ae92011ef28b4d56131c7b
saoh <saoh@student.42seoul.kr>
2022-04-09 18:15:17 +0900

    ft_container

---
+++ ft_container/enable_and_is_integral_test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "utils.hpp"
+
+template <class T, class U = typename ft::enable_if<ft::is_integral<T>::value>::type>
+struct test
+{
+    void operator()(T a)
+    {
+        std::cout << "T : " << a << std::endl;
+    }
+};
+
+int main()
+{
+    test<int> b;
+    test<char> c;
+    b(1);
+    c(49);
+    std::cout << std::boolalpha;
+    std::cout << "bool : " << ft::is_integral<bool>::value << std::endl;
+    std::cout << "char : " << ft::is_integral<char>::value << std::endl;
+    std::cout << "wchar_t : " << ft::is_integral<wchar_t>::value << std::endl;
+    std::cout << "signed char : " << ft::is_integral<signed char>::value << std::endl;
+    std::cout << "short int : " << ft::is_integral<short int>::value << std::endl;
+    std::cout << "int : " << ft::is_integral<int>::value << std::endl;
+    std::cout << "long int : " << ft::is_integral<long int>::value << std::endl;
+    std::cout << "long long int : " << ft::is_integral<long long int>::value << std::endl;
+    std::cout << "unsigned char : " << ft::is_integral<unsigned char>::value << std::endl;
+    std::cout << "unsigned short int : " << ft::is_integral<unsigned short int>::value << std::endl;
+    std::cout << "unsigned int : " << ft::is_integral<unsigned int>::value << std::endl;
+    std::cout << "unsigned long int : " << ft::is_integral<unsigned long int>::value << std::endl;
+    std::cout << "unsigned long long int : " << ft::is_integral<unsigned long long int>::value << std::endl;
+    std::cout << "double : " << ft::is_integral<double>::value << std::endl;
+    std::cout << "float : " << ft::is_integral<float>::value << std::endl;
+}
--- ft_container/utils.hpp
+++ ft_container/utils.hpp
@@ -175,6 +175,94 @@ namespace ft
     {
         return (lhs.base().base() - rhs.base().base());
     }
+
+    template<bool Cond, class T = void>
+    struct enable_if {};
+    
+    template<class T>
+    struct enable_if<true, T>
+    {
+        typedef T type;
+    };
+
+    template <class T, T v >
+    struct integral_constant
+    {
+        typedef T value_type;
+        typedef integral_constant<T,v> type;
+        static const T value = v;
+    };
+
+    typedef integral_constant<bool, true> true_type;
+    typedef integral_constant<bool, false> false_type;
+
+    template <class T>
+    struct is_integral : false_type {};
+
+    template <>
+    struct is_integral<bool> : public true_type {};
+
+    template <>
+    struct is_integral<char> : public true_type {};
+
+    template <>
+    struct is_integral<wchar_t> : public true_type {};
+
+    template <>
+    struct is_integral<signed char> : public true_type {};
+
+    template <>
+    struct is_integral<short int> : public true_type {};
+
+    template <>
+    struct is_integral<int> : public true_type {};
+
+    template <>
+    struct is_integral<long int> : public true_type {};
+
+    template <>
+    struct is_integral<long long int> : public true_type {};
+
+    template <>
+    struct is_integral<unsigned char> : public true_type {};
+
+    template <>
+    struct is_integral<unsigned short int> : public true_type {};
+
+    template <>
+    struct is_integral<unsigned int> : public true_type {};
+
+    template <>
+    struct is_integral<unsigned long int> : public true_type {};
+
+    template <>
+    struct is_integral<unsigned long long int> : public true_type {};
+
+    template <class InputIterator1, class InputIterator2>
+    bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+    {
+        while(first1 != last1)
+        {
+            if (!(*first1 == *first2))
+                return (false);
+            ++first1;
+            ++first2;
+        }
+        return (true);
+    }
+
+    template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+    bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+    {
+        while (first1 != last1)
+        {
+            if (!pred(*first1, *first2))
+                return (false);
+            ++first1;
+            ++first2;
+        }
+        return (true);
+    }
 }
 
 #endif
